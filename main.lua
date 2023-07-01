59d9e3bf794fe963082be87aa02e34b0393dd5f1
David Roque <davidroque21@hotmail.com>
2023-06-30 22:56:59 -0400

    add push library for virual screen size

--- main.lua
+++ main.lua
@@ -1,19 +1,36 @@
+push = require 'push'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+		love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
     })
 end
 
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
 function love.draw()
+    push:apply('start')
+
     love.graphics.printf(
-        'Hello Pong!',          -- text to render
+        'Hello Pong dasdas!',          -- text to render
         0,                      -- starting X (0 since we're going to center it based on width)
         WINDOW_HEIGHT / 2 - 6,  -- starting Y (halfway down the screen)
         WINDOW_WIDTH,           -- number of pixels to center within (the entire screen here)
         'center')               -- alignment mode, can be 'center', 'left', or 'right'
+
+    push:apply('end')
 end

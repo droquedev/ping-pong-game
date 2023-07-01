281a7ee375a79421262abd4ed307dc9069c1feb8
David Roque <davidroque21@hotmail.com>
2023-06-30 23:09:34 -0400

    draw paddles and add new font to the app

--- main.lua
+++ main.lua
@@ -9,6 +9,10 @@ VIRTUAL_HEIGHT = 243
 function love.load()
 		love.graphics.setDefaultFilter('nearest', 'nearest')
 
+		smallFont = love.graphics.newFont('/fonts/pixel.ttf', 18)
+
+		love.graphics.setFont(smallFont)
+
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
@@ -25,12 +29,20 @@ end
 function love.draw()
     push:apply('start')
 
+		love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+
     love.graphics.printf(
-        'Hello Pong dasdas!',          -- text to render
+        'Hello Pong',          -- text to render
         0,                      -- starting X (0 since we're going to center it based on width)
-        WINDOW_HEIGHT / 2 - 6,  -- starting Y (halfway down the screen)
-        WINDOW_WIDTH,           -- number of pixels to center within (the entire screen here)
+        20,  -- starting Y (halfway down the screen)
+        VIRTUAL_WIDTH,           -- number of pixels to center within (the entire screen here)
         'center')               -- alignment mode, can be 'center', 'left', or 'right'
 
+		love.graphics.rectangle('fill', 10, 30, 5, 20)
+
+		love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+
+		love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 5, 5)
+		
     push:apply('end')
 end

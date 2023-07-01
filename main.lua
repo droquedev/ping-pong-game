68778f9fa9af2066e2e3d68ebcfb5ad603dc95c4
David Roque <davidroque21@hotmail.com>
2023-07-01 00:03:18 -0400

    add entities for Ball and Paddle

--- main.lua
+++ main.lua
@@ -1,4 +1,9 @@
-push = require 'push'
+local push = require 'push'
+
+Class = require 'class'
+
+require 'Paddle'
+require 'Ball'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -8,100 +13,98 @@ VIRTUAL_HEIGHT = 243
 
 PADDLE_SPEED = 200
 
+local smallFont = nil
+local scoreFont = nil
+local player1 = nil
+local player2 = nil
+local ball = nil
+local gameState = nil
+
 function love.load()
-		love.graphics.setDefaultFilter('nearest', 'nearest')
+	love.graphics.setDefaultFilter('nearest', 'nearest')
 
-		math.randomseed(os.time())
+	math.randomseed(os.time())
 
-		smallFont = love.graphics.newFont('/fonts/pixel.ttf', 8)
+	smallFont = love.graphics.newFont('/fonts/pixel.ttf', 8)
 
-		scoreFont = love.graphics.newFont('/fonts/pixel.ttf', 32)
+	scoreFont = love.graphics.newFont('/fonts/pixel.ttf', 32)
 
-		love.graphics.setFont(smallFont)
+	love.graphics.setFont(smallFont)
 
-    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
-        fullscreen = false,
-        resizable = false,
-        vsync = true
-    })
+	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+		fullscreen = false,
+		resizable = false,
+		vsync = true
+	})
 
-		player1Score = 0
-		player2Score = 0
+	player1 = Paddle(10, 30, 5, 20)
+	player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
 
-		player1Y = 30
-		player2Y = VIRTUAL_HEIGHT - 50
+	ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
-		ballX = VIRTUAL_WIDTH / 2 - 2
-		ballY = VIRTUAL_HEIGHT / 2 - 2
-
-		ballDX = math.random(2) == 1 and -100 or 100
-		ballDY = math.random(-50, 50)
-
-		gameState = 'start'
+	gameState = 'start'
 end
 
 function love.update(dt)
-		if love.keyboard.isDown('w') then
-				player1Y = math.max(0, player1Y - PADDLE_SPEED * dt)
+	if love.keyboard.isDown('w') then
+		player1.dy = -PADDLE_SPEED
+	elseif love.keyboard.isDown('s') then
+		player1.dy = PADDLE_SPEED
+	else
+		player1.dy = 0
+	end
 
-		elseif love.keyboard.isDown('s') then
-				player1Y = math.min(VIRTUAL_HEIGHT - 20, player1Y + PADDLE_SPEED * dt)
-		end
+	if love.keyboard.isDown('up') then
+		player2.dy = -PADDLE_SPEED
+	elseif love.keyboard.isDown('down') then
+		player2.dy = PADDLE_SPEED
+	else
+		player2.dy = 0
+	end
 
-		if love.keyboard.isDown('up') then
-				player2Y = math.max(0, player2Y - PADDLE_SPEED * dt)
-		elseif love.keyboard.isDown('down') then
-				player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * dt)
-		end
+	if gameState == 'play' then
+		ball:update(dt)
+	end
 
-		if gameState == 'play' then
-				ballX = ballX + ballDX * dt
-				ballY = ballY + ballDY * dt
-		end
+	player1:update(dt)
+	player2:update(dt)
 end
 
 function love.keypressed(key)
-    if key == 'escape' then
-        love.event.quit()
-    end
+	if key == 'escape' then
+		love.event.quit()
+	end
 
-		if key == 'enter' or key == 'return' then
-				if gameState == 'start' then
-						gameState = 'play'
-				else
-						gameState = 'start'
+	if key == 'enter' or key == 'return' then
+		if gameState == 'start' then
+			gameState = 'play'
+		else
+			gameState = 'start'
 
-						ballX = VIRTUAL_WIDTH / 2 - 2
-						ballY = VIRTUAL_HEIGHT / 2 - 2
-
-						ballDX = math.random(2) == 1 and -100 or 100
-						ballDY = math.random(-50, 50)
-				end
+			ball:reset()
 		end
+	end
 end
 
 function love.draw()
-    push:apply('start')
+	push:apply('start')
 
-		love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+	love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
 
-		love.graphics.setFont(smallFont)
-		if gameState == 'start' then
-				love.graphics.printf('Hello Start State!', 0, 20, VIRTUAL_WIDTH, 'center')
-		else
-				love.graphics.printf('Hello Play State!', 0, 20, VIRTUAL_WIDTH, 'center')
-		end
-		
+	love.graphics.setFont(smallFont)
+	if gameState == 'start' then
+		love.graphics.printf('Hello Start State!', 0, 20, VIRTUAL_WIDTH, 'center')
+	else
+		love.graphics.printf('Hello Play State!', 0, 20, VIRTUAL_WIDTH, 'center')
+	end
 
-		love.graphics.setFont(scoreFont)
-		love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
-		love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
 
-		love.graphics.rectangle('fill', 10, player1Y, 5, 20)
+	player1:render()
+	player2:render()
 
-		love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
+	ball:render()
 
-		love.graphics.rectangle('fill', ballX, ballY, 4, 4)
-		
-    push:apply('end')
+	love.graphics.setFont(scoreFont)
+
+	push:apply('end')
 end

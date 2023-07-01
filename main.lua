eafe0f6d3dde92dcd64d2cd54861523b849d0a5f
David Roque <davidroque21@hotmail.com>
2023-07-01 01:15:36 -0400

    add sounds

--- main.lua
+++ main.lua
@@ -24,6 +24,7 @@ local player2Score = 0
 local servingPlayer = 1
 local largeFont = nil
 local winningPlayer = 0
+local sounds = nil
 
 function love.load()
 	love.window.setTitle("David's Pong Game")
@@ -34,6 +35,12 @@ function love.load()
 	smallFont = love.graphics.newFont('/fonts/pixel.ttf', 8)
 	largeFont = love.graphics.newFont('/fonts/pixel.ttf', 16)
 	scoreFont = love.graphics.newFont('/fonts/pixel.ttf', 32)
+
+	sounds = {
+		['paddle_hit'] = love.audio.newSource('/sounds/paddle_hit.wav', 'static'),
+		['score'] = love.audio.newSource('/sounds/score.wav', 'static'),
+		['wall_hit'] = love.audio.newSource('/sounds/wall_hit.wav', 'static')
+	}
 
 	love.graphics.setFont(smallFont)
 
@@ -72,6 +79,8 @@ function love.update(dt)
 			else
 				ball.dy = math.random(10, 150)
 			end
+
+			sounds['paddle_hit']:play()
 		end
 
 		if ball:collides(player2) then
@@ -83,16 +92,22 @@ function love.update(dt)
 			else
 				ball.dy = math.random(10, 150)
 			end
+
+			sounds['paddle_hit']:play()
 		end
 
 		if ball.y <= 0 then
 			ball.y = 0
 			ball.dy = -ball.dy
+
+			sounds['wall_hit']:play()
 		end
 
 		if ball.y >= VIRTUAL_HEIGHT - 4 then
 			ball.y = VIRTUAL_HEIGHT - 4
 			ball.dy = -ball.dy
+
+			sounds['wall_hit']:play()
 		end
 
 		ball:update(dt)
@@ -101,6 +116,7 @@ function love.update(dt)
 			servingPlayer = 1
 			player2Score = player2Score + 1
 
+			sounds['score']:play()
 			if player2Score >= 10 then
 				winningPlayer = 2
 				gameState = 'done'
@@ -113,6 +129,8 @@ function love.update(dt)
 		if ball.x > VIRTUAL_WIDTH then
 			servingPlayer = 2
 			player1Score = player1Score + 1
+
+			sounds['score']:play()
 			if player1Score >= 10 then
 				winningPlayer = 1
 				gameState = 'done'
